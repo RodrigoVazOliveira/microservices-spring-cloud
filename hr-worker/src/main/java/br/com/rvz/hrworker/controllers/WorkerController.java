96d6f308965c70691be3e5af14912fe3d36a7ae1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-08-14 18:21:53 -0300

    Criar busca por id de workers

--- hr-worker/src/main/java/br/com/rvz/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/rvz/hrworker/controllers/WorkerController.java
@@ -1,10 +1,14 @@
 package br.com.rvz.hrworker.controllers;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.rvz.hrworker.entities.Worker;
 import br.com.rvz.hrworker.repositories.WorkerRepository;
@@ -23,5 +27,17 @@ public class WorkerController {
 	@ResponseStatus(HttpStatus.OK)
 	public Iterable<Worker> getAllWorkers() {
 		return this.workerRepository.findAll();
+	}
+	
+	@GetMapping("/{id}/")
+	@ResponseStatus(HttpStatus.OK)
+	public Worker findWorkerById(@PathVariable(name = "id") Long id) {
+		Optional<Worker> optionalWorker = workerRepository.findById(id);
+		
+		if (optionalWorker.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trabalhador não encontrado com ID " + id);
+		}
+		
+		return optionalWorker.get();
 	}
 }
