5990c733140610bac5232acf902013fec8aa17ec
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-08-15 13:14:29 -0300

    feito comunicação entre os dois projeto

--- hr-payroll/src/main/java/br/com/rvz/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/rvz/hrpayroll/services/PaymentService.java
@@ -1,16 +1,37 @@
 package br.com.rvz.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.rvz.hrpayroll.entities.Payment;
+import br.com.rvz.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String hrWorkerHost;
+	
+	private RestTemplate restTemplate;
+	
+	public PaymentService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
 	public Payment getPayment(Long workerId, Integer days) {
+		Map<String, String> uriVarable = new HashMap<>();
+		uriVarable.put("id", workerId.toString());
+		
+		Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}/",
+				Worker.class, uriVarable);
+		
 		Payment payment = new Payment();
-		payment.setName("Bob");
-		payment.setDailyincome(200.00);
+		payment.setName(worker.getName());
+		payment.setDailyincome(worker.getDailyIncome());
 		payment.setDays(days);
 		
 		return payment;
