e4ccb0628aa2c24a4fe01f2b64421441886e03a0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-08-15 22:53:33 -0300

    configurado circuit break

--- hr-payroll/src/main/java/br/com/rvz/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/br/com/rvz/hrpayroll/controllers/PaymentController.java
@@ -1,14 +1,14 @@
 package br.com.rvz.hrpayroll.controllers;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rvz.hrpayroll.entities.Payment;
 import br.com.rvz.hrpayroll.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/payments/")
@@ -19,10 +19,19 @@ public class PaymentController {
 	public PaymentController(PaymentService paymentService) {
 		this.paymentService = paymentService;
 	}
-
+	
+	@CircuitBreaker(name = "hr-worker", fallbackMethod = "getPaymentsAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}/")
-	@ResponseStatus(HttpStatus.OK)
-	public Payment getPayment(@PathVariable(name = "workerId") Long workerId, @PathVariable(name = "days") Integer days) {
-		return paymentService.getPayment(workerId, days);
+	public ResponseEntity<Payment> getPayment(@PathVariable(name = "workerId") Long workerId, @PathVariable(name = "days") Integer days) {
+		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentsAlternative(Long workerId, Integer days, RuntimeException ex) {
+		Payment payment = new Payment();
+		payment.setName("NoName");
+		payment.setDailyincome(1.0);
+		payment.setDays(1);
+		
+		return ResponseEntity.ok(payment);	
 	}
 }
