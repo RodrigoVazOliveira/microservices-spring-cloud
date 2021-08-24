a6ef2091a2cd1f16bb3049d5b6e52727c2845b21
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-08-23 23:09:43 -0300

    criar novo projeto de gateway

---
+++ hr-api-gateway-zuul/src/test/java/br/com/rvz/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rvz.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
