84cde2415268bfc33e374d44514492e775cee72c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-07 06:45:36 -0300

    ignorar configurações de IDE

--- hr-api-gateway-zuul/src/main/java/br/com/rvz/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/br/com/rvz/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+    }
 
 }
