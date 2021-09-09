02a882f36c1e04a92509f4c2c86000c662ab9fdc
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-09 09:43:01 -0300

    Criar entidades

--- hr-oauth/src/main/java/br/com/rvz/hroauth/entities/Role.java
+++ hr-oauth/src/main/java/br/com/rvz/hroauth/entities/Role.java
@@ -1,2 +1,25 @@
-package br.com.rvz.hroauth.entities;public class Role {
+package br.com.rvz.hroauth.entities;
+
+public class Role {
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
