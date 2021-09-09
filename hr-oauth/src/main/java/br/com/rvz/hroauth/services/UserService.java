684bd3cc65c257bebb873536a258df7fa3f438f1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-09 09:43:17 -0300

    Criar regra de negocio para buscar usuarios

--- hr-oauth/src/main/java/br/com/rvz/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/rvz/hroauth/services/UserService.java
@@ -1,2 +1,29 @@
-package br.com.rvz.hroauth.services;public class UserService {
+package br.com.rvz.hroauth.services;
+
+import br.com.rvz.hroauth.entities.User;
+import br.com.rvz.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserService.class);
+    private final UserFeignClient userFeignClient;
+
+    public UserService(UserFeignClient userFeignClient) {
+        this.userFeignClient = userFeignClient;
+    }
+
+    public User findByEmail(String email) {
+        User user = userFeignClient.findByEmail(email);
+
+        if (user == null) {
+            log.error("findByEmail email: {}", email);
+            throw new IllegalArgumentException("Usuário com e-mail " + email + "não foi localizado!");
+        }
+
+        return user;
+    }
 }
