6bea039d0b7bcb49070d8d7d6a86e865c341b6ed
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-10 07:23:25 -0300

    adicoinar dependencia de oauth2

---
+++ hr-oauth/src/main/java/br/com/rvz/hroauth/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package br.com.rvz.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public BCryptPasswordEncoder buildBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
