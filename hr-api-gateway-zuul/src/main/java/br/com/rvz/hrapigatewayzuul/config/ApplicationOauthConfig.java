184b54bb804314f24f6c6c4ea6d3ba924d796e5b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-16 08:46:12 -0300

    configurado websecurity

---
+++ hr-api-gateway-zuul/src/main/java/br/com/rvz/hrapigatewayzuul/config/ApplicationOauthConfig.java
@@ -0,0 +1,23 @@
+package br.com.rvz.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class ApplicationOauthConfig {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("mysecret-key");
+
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+}
