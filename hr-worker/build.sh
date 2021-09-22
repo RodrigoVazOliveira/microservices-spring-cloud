24c9cde32dc00ea18679dfd6895920a8e33661d8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-09-22 01:24:43 -0300

    criar script para fazer deploy para testes em desenvolvimento

--- hr-worker/build.sh
+++ hr-worker/build.sh
@@ -1,4 +1,3 @@
 mvn clean package -Dmaven.test.skip
 sudo docker build -t hr-worker:v1 .
-sudo docker rm hr-worker
-sudo docker run -P --name hr-worker --network rh-rede-principal hr-worker:v1  &
+sudo docker run -P --network rh-rede-principal hr-worker:v1  &
